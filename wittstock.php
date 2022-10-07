0a06725cb6d1eaaef4e24bc432e66d20aba34deb
Steffen Schultz <steffenschultz@mailbox.org>
2022-10-07 13:28:43 +0200

    Initial commit

---
+++ wittstock.php
@@ -0,0 +1,23 @@
+<?php
+// Wittstock extension, https://github.com/schulle4u/yellow-wittstock
+
+class YellowWittstock {
+    const VERSION = "0.8.21";
+    public $yellow;         // access to API
+    
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+    }
+    
+    // Handle update
+    public function onUpdate($action) {
+        $fileName = $this->yellow->system->get("coreExtensionDirectory").$this->yellow->system->get("coreSystemFile");
+        if ($action=="install") {
+            $this->yellow->system->save($fileName, array("theme" => "wittstock"));
+        } elseif ($action=="uninstall" && $this->yellow->system->get("theme")=="wittstock") {
+            $theme = reset(array_diff($this->yellow->system->getValues("theme"), array("wittstock")));
+            $this->yellow->system->save($fileName, array("theme" => $theme));
+        }
+    }
+}

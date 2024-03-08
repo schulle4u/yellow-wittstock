c1a1fc1ed72a5fe2b161dd932bfa762154268c3a
Steffen Schultz <steffenschultz@mailbox.org>
2024-03-08 15:57:59 +0100

    Updated simple.css to v2.3.0

--- wittstock.php
+++ wittstock.php
@@ -2,7 +2,7 @@
 // Wittstock extension, https://github.com/schulle4u/yellow-wittstock
 
 class YellowWittstock {
-    const VERSION = "0.8.28";
+    const VERSION = "0.8.29";
     public $yellow;         // access to API
     
     // Handle initialisation
